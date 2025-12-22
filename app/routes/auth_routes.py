b0a040d5e50b16bbd87d8866730dd3f513d3cfb0
masha-lagutkina <lagutkinamasa1@gmail.com>
2025-12-22 22:33:00 +0300

    Update auth_routes.py

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -1,4 +1,3 @@
-app/routes/auth_routes.py
 from flask import Blueprint, render_template, redirect, url_for, flash, request
 from flask_login import login_user, logout_user, current_user
 from app import db
